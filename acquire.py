baf3ae20913ea55d3f005e429a2fca5c5f2202bb
Chad Allen <chad.allen.210@gmail.com>
2021-05-14 10:00:04 -0500

    adding acquire.py file

---
+++ acquire.py
@@ -0,0 +1,56 @@
+##### IMPORTS #####
+import pandas as pd
+import numpy as np
+import os
+from env import host, username, password
+
+##### CONNECT TO CODEUP DB #####
+def get_db_url(db, username=username, host=host, password=password):
+    
+    return f'mysql+pymysql://{username}:{password}@{host}/{db}'
+
+##### ACQUIRE TITANIC DATA FUNCTIONS #####
+def new_titanic_data():
+    
+    sql_query_titanic = 'SELECT * FROM passengers'
+    
+    df = pd.read_sql(sql_query_titanic, get_db_url('titanic_db'))
+    
+    return df
+
+def get_titanic_data(cached=False):
+    
+    if cached == False or os.path.isfile('titanic_df.csv') == False:
+        
+        df = new_titanic_data()
+        
+        df.to_csv('titanic_df.csv')
+    
+    else:
+        
+        df = pd.read_csv('titanic_df.csv', index_col=0)
+    
+    return df
+
+##### ACQUIRE IRIS DATA FUNCTIONS #####
+def new_iris_data():
+    
+    sql_query_iris = 'SELECT * FROM measurements JOIN species USING (species_id)'
+                
+    df = pd.read_sql(sql_query_iris, get_db_url('iris_db'))
+    
+    return df
+
+def get_iris_data(cached=False):
+    
+    if cached == False or os.path.isfile('iris_df.csv') == False:
+        
+        df = new_iris_data()
+        
+        df.to_csv('iris_df.csv')
+    
+    else:
+        
+        df = pd.read_csv('iris_df.csv', index_col=0)
+    
+    return df
