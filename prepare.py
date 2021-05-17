b7167d9905c97c055407ee653a77602993d50e51
Chad Allen <chad.allen.210@gmail.com>
2021-05-17 09:39:41 -0500

    updates

--- prepare.py
+++ prepare.py
@@ -9,8 +9,6 @@ import acquire
 
 def prep_iris(df):
     
-    df = acquire.get_iris_data()
-    
     df = df.drop(columns=['species_id', 'measurement_id'])
     
     df.rename(columns={'species_name': 'species'}, inplace=True)
