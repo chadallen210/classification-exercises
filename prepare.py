bb31fa43223169e1730568795475679265aee9fd
Chad Allen <chad.allen.210@gmail.com>
2021-05-20 15:14:25 -0500

    update

--- prepare.py
+++ prepare.py
@@ -16,3 +16,30 @@ def prep_iris(df):
     df = pd.get_dummies(df, columns=['species'], drop_first=True)
     
     return df
+
+def prep_titanic(df):
+    '''
+    This function take in the titanic data acquired by get_titanic_data,
+    Returns prepped train, validate, and test dfs with embarked dummy vars,
+    deck dropped, and the mean of age imputed for Null values.
+    '''
+    
+    # drop rows where embarked/embark town are null values
+    df = df[~df.embarked.isnull()]
+    
+    # encode embarked using dummy columns
+    titanic_dummies = pd.get_dummies(df.embarked, drop_first=True)
+    
+    # join dummy columns back to df
+    df = pd.concat([df, titanic_dummies], axis=1)
+    
+    # drop the deck column
+    df = df.drop(columns='deck')
+    
+    # split data into train, validate, test dfs
+    train, validate, test = titanic_split(df)
+    
+    # impute mean of age into null values in age column
+    train, validate, test = impute_mean_age(train, validate, test)
+    
+    return train, validate, test
